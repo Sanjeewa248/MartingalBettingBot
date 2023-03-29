89d0170b3ee46dc951caca51b6c624955ac07026
Sanjeewa_SL <sanjeewa.kumara@blockstarsglobal.com>
2023-03-29 16:55:06 +0530

    CHG | Random bet and save profit,loss all game result to different json

--- Martin and paroly system/MartinParoliSystemBetting.py
+++ Martin and paroly system/MartinParoliSystemBetting.py
@@ -14,6 +14,7 @@ class User:
         self.lose_cnt = 0
         self.draw_cnt = 0
         self.streak = 0
+        self.profit_loss = 0
 
     def betting(self):
         if self.balance - self.bet > 0:
@@ -69,11 +70,14 @@ class User:
     def print_cnt(self):
         print(f'name : {self.name}, win : {self.win_cnt}, lose : {self.lose_cnt}, draw : {self.draw_cnt}, streak : {self.streak}')
 
+    def calculate_profit_loss(self):
+        self.profit_loss = self.balance - self.ori_bet
+
 
 
 user1 = User('Daniel', 1000000, 5000, 5000, 'player', 'paroli')
-user2 = User('Jaina', 100000, 5000, 5000, 'player', 'paroli')
-user3 = User('John', 100000, 5000, 5000, 'banker', 'paroli')
+user2 = User('Jaina', 1000000, 5000, 5000, 'player', 'paroli')
+user3 = User('John', 1000000, 5000, 5000, 'banker', 'martin')
 
 users = [user1, user2, user3]
 # users.append(user3)
@@ -86,8 +90,8 @@ print('first user information:')
 for user in users:
     user.print_status()
 # play game for 19 rounds
-for i in range(19):
-    time.sleep(2)
+for i in range(500):
+    # time.sleep(2)
     round_num += 1
     result = {'round': round_num, 'winner': random.choice(winning_results)}
     game_results.append(result)
@@ -118,6 +122,34 @@ print("Final balances:")
 for user in users:
     user.print_status()
     user.print_cnt()
+
+# calculate profit/loss for each user and save to a JSON file
+user_profits = []
+for user in users:
+    user.calculate_profit_loss()
+    profit_loss = user.profit_loss
+    profit_loss_type = 'profit' if profit_loss > 0 else 'loss'
+    user_profit = {'name': user.name, 'balance': user.balance, 'profit_loss': profit_loss, 'type': profit_loss_type, 'strategy': user.strategy}
+    user_profits.append(user_profit)
+
+with open('result.json', 'w') as f:
+    json.dump(user_profits, f)
+
+
+
+
+
+for user in users:
+    user.calculate_profit_loss()
+    print(f"{user.name} {'profit' if user.profit_loss > 0 else 'loss'}: {user.profit_loss}")
+
+
+
+
+
+
+
+
 
 
 
